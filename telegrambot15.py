2cd1f6f80f645a8f3991cf98cf2729869d56973e
Nursultan15 <73534336+Nursultan15@users.noreply.github.com>
2020-11-05 15:49:16 +0600

    Add files via upload
    First, I want to say about the capabilities of my bot and what it can do, what commands we can enter so that the bot works correctly. In short, I created this bot to help students like me. With this bot we can quickly find all the lessons on the site. The names of our teachers and how to contact them. You see, during an online class, many students are late for class. Because of this, we start the lesson 5 or 10 minutes late. I don’t blame them, because it’s not easy to find tutorials and links to zoom on the site. And it is still not easy to contact teachers. That's why I wanted to create such a bot.
     Press any word and the bot will enter to us:
    I do not have such kind of command
    If you get bored click on / entertainment
    if you need help click on / help
    As you can see, the bot will offer us two commands
    Well, if you are tired and want to relax, then click on the first command / entertainment and it will offer us all sorts of interesting YouTube channels. For example football, news, animal world, movies or video bloggers. Well, this is mainly if you are already very tired or bored.
    And if you want to immediately get to work, then click on the command / help.
    There will appear 4 commands. For example, if you want to find lessons in the OCC, then click on the first command and it will show a list of all lessons in the OCS. There you will choose what you need, and there you will see a link to the lesson, the teacher's name and the teacher's mail.
    In short, it is very easy to understand.
    And other teams are like additional lessons. There are commands / python_language, / html_language and / creat_a_bot.
    As you understand, 2 is a command for learning python, and 3 is a command for html. Press the 4-command if you want to create your own bot by yourself, I think this will make the work of many students easier. That's all for now. That's all i could do

---
+++ telegrambot15.py
@@ -0,0 +1,234 @@
+import telebot
+
+token = '1094865183:AAGH3s7ND6P0iH6oIWDsu4I9-QgW3SeChf4'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hello , '
+                                      'I will help you find lessons in ocs')
+    bot.send_message(message.chat.id, 'What do you need?')
+    bot.send_message(message.chat.id, '1 /english')
+    bot.send_message(message.chat.id, '2 /ecology')
+    bot.send_message(message.chat.id, '3 /python')
+    bot.send_message(message.chat.id, '4 /intro2eng')
+    bot.send_message(message.chat.id, '5 /physics')
+    bot.send_message(message.chat.id, '6 /calculus')
+    bot.send_message(message.chat.id, '7 /russian')
+    bot.send_message(message.chat.id, '8 /turkish')
+    bot.send_message(message.chat.id, '9 /physicalculture')
+
+
+@bot.message_handler(commands=['settings'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'There is no problem here\n'
+                                      'Click on /help')
+
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'If you want to start the program,\n'
+                                      'press the /start command!\n\n'
+                                      'If you need help with python,\n'
+                                      'press to /python_language command!\n\n'
+                                      'If you need help with HTML,\n'
+                                      'press to /html_language command!\n\n'
+                                      'If you want to create '
+                                      'a telegram bot like me,\n'
+                                      'press to /create_a_bot command!')
+
+
+@bot.message_handler(commands=['entertainment'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'WHAT DO YOU PREFER?')
+    bot.send_message(message.chat.id, '1) /football')
+    bot.send_message(message.chat.id, '2) /news')
+    bot.send_message(message.chat.id, '3) /nat_geo_wild')
+    bot.send_message(message.chat.id, '4) /films')
+    bot.send_message(message.chat.id, '5) /gamers')
+
+
+@bot.message_handler(commands=['football'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Football:'
+                                      'https://www.'
+                                      'youtube.com/c/TARAFTARKANALIHD/videos')
+    bot.send_message(message.chat.id, 'https://www.youtube.com/channel/'
+                                      'UCO8qj5u80Ga7N_tP3BZWWhQ/videos')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'youtube.com/c/AllFootball28/videos')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'youtube.com/c/Score90/videos')
+
+
+@bot.message_handler(commands=['news'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'News: '
+                                      'https://www.'
+                                      'youtube.com/c/NBCNews/videos')
+    bot.send_message(message.chat.id, 'https://www.youtube.com/c/'
+                                      'BBCNews/videos')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'youtube.com/c/globalnews/videos')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'youtube.com/c/ddnews/videos')
+
+
+@bot.message_handler(commands=['nat_geo_wild'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Nat Geo Wild: '
+                                      'https://www.youtube.com/channel'
+                                      '/UChqjsr9DovXmAUtS_q_2V0w/videos')
+
+
+@bot.message_handler(commands=['films'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Films'
+                                      ':https://www.'
+                                      'youtube.com/c/FilmsterRu/videos')
+    bot.send_message(message.chat.id, 'https://www.youtube.'
+                                      'com/c/RVisionChannel/videos')
+    bot.send_message(message.chat.id, 'https://www.youtube.com/channel/'
+                                      'UCK3AJaBVixx5BvCPLI2Ho9g/videos')
+
+
+@bot.message_handler(commands=['gamers'])
+def start_message(message):
+    bot.send_message(message.chat.id, '1) Marmok:\n\n'
+                                      'https://www.'
+                                      'youtube.com/c/MrMarmok/videos')
+    bot.send_message(message.chat.id, '2) Joe speen:\n\n'
+                                      'https://www.'
+                                      'youtube.com/c/JoeSpeen/videos')
+    bot.send_message(message.chat.id, '3) Quantum:\n\n'
+                                      'https://www.'
+                                      'youtube.com/c/QuantumGames/videos')
+    bot.send_message(message.chat.id, '4) Buster:\n\n'
+                                      'https://www.youtube.'
+                                      'com/c/%D0%91%D1%83%D1%81%'
+                                      'D1%82%D0%B5%D1%80/videos')
+
+
+@bot.message_handler(commands=['english'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'English:\n'
+                                      'https://classroom.'
+                                      'google.com/c/MTU5MjczNzc1MDk1')
+    bot.send_message(message.chat.id, 'Teacher: Sagyn Basnyat')
+    bot.send_message(message.chat.id, 'Gmail: ')
+
+
+@bot.message_handler(commands=['ecology'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Ecology:\n'
+                                      'https://classroom.'
+                                      'google.com/c/MTUzNjg2NTcxMTM5')
+    bot.send_message(message.chat.id, 'Teacher: Turdukan Bekimbetova')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      'turdukan.bekimbetova@iaau.edu.kg')
+
+
+@bot.message_handler(commands=['python'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Python:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=28')
+    bot.send_message(message.chat.id, 'Teachers: '
+                                      'Ruslan Isaev and Gulzada Esenalieva')
+    bot.send_message(message.chat.id, 'Gmail: ruslan.isaev@alatoo.edu.kg')
+    bot.send_message(message.chat.id, 'Gmail: ruslan.isaev@iaau.edu.kg')
+    bot.send_message(message.chat.id, 'Gmail: gulzada.'
+                                      'esenalieva@alatoo.edu.kg')
+
+
+@bot.message_handler(commands=['intro2eng'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Intro2eng:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=2')
+    bot.send_message(message.chat.id, 'Teacher: Burul Shambetova')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      'burul.shambetova@alatoo.edu.kg')
+
+
+@bot.message_handler(commands=['physics'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Physics:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=135')
+    bot.send_message(message.chat.id, 'Teacher: Tahir Aslan')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      't.aslan@alatoo.edu.kg')
+
+
+@bot.message_handler(commands=['calculus'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Calculus:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=193')
+    bot.send_message(message.chat.id, 'Teacher: Khalid Mohammad')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      'khalid.mohammad@alatoo.edu.kg')
+
+
+@bot.message_handler(commands=['russian'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Russian:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=409')
+    bot.send_message(message.chat.id, 'Teacher: Surakan Muzurbekovna')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      'surakan.alymbekova@iaau.edu.kg')
+
+
+@bot.message_handler(commands=['turkish'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Turkish:\n'
+                                      'https://ocs.iaau.edu.kg/'
+                                      'course/view.php?id=1550')
+    bot.send_message(message.chat.id, 'Teacher: Mr Mehmet')
+    bot.send_message(message.chat.id, 'Gmail: ')
+
+
+@bot.message_handler(commands=['physicalculture'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Physicalculture:\n'
+                                      'https: //ocs.iaau.edu.kg/'
+                                      'course/view.php?id=1217')
+    bot.send_message(message.chat.id, 'Teacher: '
+                                      'Irina Chalova')
+    bot.send_message(message.chat.id, 'Gmail: '
+                                      'irina.chalova@iaau.edu.kg')
+
+
+@bot.message_handler(commands=['python_language'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Here you can learn to'
+                                      ' python for free.')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'w3schools.com/python/')
+
+
+@bot.message_handler(commands=['html_language'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Here you can learn to'
+                                      ' html for free.')
+    bot.send_message(message.chat.id, 'https://www.'
+                                      'w3schools.com/html/default.asp')
+
+
+@bot.message_handler(commands=['create_a_bot'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Here you can create a new bot.')
+    bot.send_message(message.chat.id, 'https://t.me/BotFather')
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, 'I do not have such kind of command')
+    bot.send_message(message.chat.id, 'If you get bored click on'
+                                      ' /entertainment')
+    bot.send_message(message.chat.id, 'if you need help click on /help')
+
+bot.polling()
